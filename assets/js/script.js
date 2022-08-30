ac656fdfacb2684891fb6a398f60b15161cec20d
505laura <106522200+505laura@users.noreply.github.com>
2022-08-31 03:08:50 +1000

    Add prompts to ask the user which characters they wish to include in their password

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,16 @@ const specialChars = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
 
 // Create the function that will generate a password when called
 function generatePassword() {
+   // Create pop up messages that the user can respond to with 'Ok' or 'Cancel' with questions about what characters to include 
+   const upper = confirm('Would you like to include uppercase letters in your password?');
+   const lower = confirm('Would you like to include lowercase letters in your password?');
+   const number = confirm('Would you like to include numbers in your password?');
+   const special = confirm('Would you like to include special characters in your password?');
+   // Create an alert asking the user to choose one of the above options if none were chosen
+   if (!upper && !lower && !number && !special) {
+       alert('Please choose at least one of the options.');
+       return '';
+   }
 }
 
 // Write password to the #password input
